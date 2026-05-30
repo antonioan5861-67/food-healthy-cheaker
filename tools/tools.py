6ae071f7207dc0b4f134a980dd570cb9a04e8615
antonioan5861-67 <antonioan5861@gmail.com>
2026-05-30 12:42:09 -0700

    dict fixes

--- tools/tools.py
+++ tools/tools.py
@@ -1,14 +1,14 @@
 from core.state import STATE
 
 def food_output(food: str):
-    return STATE[food]
+    return STATE["food"][food]
     
 def fat_checker(food: str):
-    return STATE[food] + "the food can be bad for you if its more than a hundred because the food fat s hight."
+    return STATE["food"][food] + "the food can be bad for you if its more than a hundred because the food fat s hight."
 
 def food_validator(food: str):
     
-    if food in STATE:
+    if food in STATE[food]:
         return("this food is in the state")
     else:
         return ("error 404 not valid")
